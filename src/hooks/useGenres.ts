f0731fc715496dfc917a0040fa818ea194d62783
th3mon <p.szelenberger@gmail.com>
2026-09-14 06:37:14 +0200

    feat: add initial genre data

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,6 +1,7 @@
 import apiClient from "@/services/api-client";
 import { useQuery } from "@tanstack/react-query";
 import type { FetchResponse } from "./useGames";
+import genres from "../data/genres";
 
 export interface Genre {
   id: number;
@@ -11,9 +12,12 @@ export interface Genre {
 const useGenres = () =>
   useQuery<FetchResponse<Genre>, Error>({
     queryKey: ["genres"],
-    queryFn: () =>
-      apiClient.get<FetchResponse<Genre>, Error>("/genres").then((response) => response.data),
+    queryFn: () => apiClient.get<FetchResponse<Genre>>("/genres").then((response) => response.data),
     staleTime: 86400000, // 24 hours in miliseconds
+    initialData: {
+      count: genres.length,
+      results: genres,
+    },
   });
 
 export default useGenres;
