200bbfe236f02c0de2b1606f716246e3b1ec1c52
th3mon <p.szelenberger@gmail.com>
2026-09-14 05:48:11 +0200

    chore: add React Query devtools

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import "./index.css";
 import { Provider } from "./components/ui/provider.tsx";
 import App from "./App.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient();
 
@@ -12,6 +13,7 @@ createRoot(document.getElementById("root")!).render(
     <QueryClientProvider client={queryClient}>
       <Provider>
         <App />
+        <ReactQueryDevtools />
       </Provider>
     </QueryClientProvider>
   </StrictMode>,
