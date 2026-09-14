ebb5574a19bf32719b5117b244667568111c28d4
th3mon <p.szelenberger@gmail.com>
2026-09-14 05:32:25 +0200

    feat: add React Query provider

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,16 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { Provider } from "./components/ui/provider.tsx";
 import App from "./App.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Provider>
-      <App />
-    </Provider>
+    <QueryClientProvider client={queryClient}>
+      <Provider>
+        <App />
+      </Provider>
+    </QueryClientProvider>
   </StrictMode>,
 );
